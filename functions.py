13e8be1d4f725d4a6e64623fa9b7404a9b8d9260
Bryan Juárez <if722176@iteso.mx>
2022-06-20 01:35:06 -0500

    Final functions definition for APT

--- functions.py
+++ functions.py
@@ -16,7 +16,7 @@ import pandas as pd
 import numpy as np
 import datetime
 
-# =================================== APT model check functions ========================================== #
+# =================================== APT model check functions =========================================== #
 
 ### Function definition for all orders
 def apt_check_all(ob_data):
@@ -37,7 +37,7 @@ def apt_check_all(ob_data):
         sub_data = ob_data[i]
         sub_data['mid_price'] = (sub_data['bid'] + sub_data['ask'])*0.5 # Simple mid-price
         sub_data['weighted_mid'] = round(w_mid(sub_data['bid'], sub_data['bid_size'],
-                                               sub_data['ask'], sub_data['ask_size'], 1), 2)
+                                               sub_data['ask'], sub_data['ask_size'], len(sub_data)), 2)
 
         new_i = pd.to_datetime(i) # Datetime type index
         sub_data.index = [datetime.datetime(new_i.year, new_i.month,
@@ -83,12 +83,14 @@ def apt_check_all(ob_data):
     simple_mid['Exp 1'] = [i[0] for i in exp1_list]
     simple_mid['Exp 2'] = [i[0] for i in exp2_list]
     simple_mid['P. Exp 1'] = np.round(np.array(simple_mid['Exp 1'])/np.array(total_list),2)
+    simple_mid['P. Exp 2'] = 1 - simple_mid['P. Exp 1']
     simple_mid.index = pd.Series(final_df.index.values).unique()
     simple_mid.index.name = 'Time'
 
     weighted_mid['Exp 1'] = [i[1] for i in exp1_list]
     weighted_mid['Exp 2'] = [i[1] for i in exp2_list]
     weighted_mid['P. Exp 1'] = np.round(np.array(weighted_mid['Exp 1']) / np.array(total_list), 2)
+    weighted_mid['P. Exp 2'] = 1 - weighted_mid['P. Exp 1']
     weighted_mid.index = pd.Series(final_df.index.values).unique()
     weighted_mid.index.name = 'Time'
 
@@ -103,6 +105,8 @@ def apt_check_top(ob_data):
     calc_inbalace = lambda b, a, d: np.sum(b[:d]) / np.sum(np.add(b[:d], a[:d]))
     w_mid = lambda b,bv,a,av,d: (bv[:d]/np.add(bv[:d],av[:d]))*a[:d]+(av[:d]/np.add(bv[:d],av[:d]))*b[:d]
     apt_check = lambda df: sum(df['Simple Mid-Price'].shift() == df['Simple Mid-Price'])
+    apt_check_a = lambda df: sum(df['Weighted Mid-Price A'].shift() == df['Weighted Mid-Price A'])
+    apt_check_b = lambda df: sum(df['Weighted Mid-Price B'].shift() == df['Weighted Mid-Price B'])
 
 # =================================== APT model check functions ========================================== #
 
@@ -111,7 +115,7 @@ def apt_check_top(ob_data):
 
                                        round((calc_inbalace(ob_data[i]['bid_size'],
                                                             ob_data[i]['ask_size'],
-                       len(ob_data[i])))*[(ob_data[i].iloc[0,:]['ask']+ob_data[i].iloc[0,:]['bid'])]*0.5,2),
+                       len(ob_data[i])))*((ob_data[i].iloc[0,:]['ask']+ob_data[i].iloc[0,:]['bid'])*0.5),2),
 
                                             round(w_mid(ob_data[i]['bid'], ob_data[i]['bid_size'],
                                                    ob_data[i]['ask'], ob_data[i]['ask_size'],1)[0], 2)]
@@ -122,7 +126,49 @@ def apt_check_top(ob_data):
     price_df.columns = ['Simple Mid-Price', 'Weighted Mid-Price A', 'Weighted Mid-Price B']
 
     # -- Grouping by 1 minute frequency -- #
-    # First list defined for
-    apt_mid = price_df.groupby(pd.Grouper('1min')).apply(apt_check).tolist()
+    # First data frame defined for simple mid-price
+    apt_mid_e1 = price_df.groupby(pd.Grouper(freq='1min')).apply(apt_check).tolist()
+    apt_mid_total = price_df.groupby(pd.Grouper(freq='1min')).count()['Simple Mid-Price']
+    apt_mid_e2 = (np.array(apt_mid_total) - np.array(apt_mid_e1)).tolist()
 
-    return apt_mid
+    # Second data frame defined for weighted mid-price a
+    apt_wmida_e1 = price_df.groupby(pd.Grouper(freq='1min')).apply(apt_check_a).tolist()
+    apt_wmida_e2 = (np.array(apt_mid_total) - np.array(apt_wmida_e1)).tolist()
+
+    # Third data frame defined for weighted mid-price b
+    apt_wmidb_e1 = price_df.groupby(pd.Grouper(freq='1min')).apply(apt_check_b).tolist()
+    apt_wmidb_e2 = (np.array(apt_mid_total) - np.array(apt_wmidb_e1)).tolist()
+
+    # -- Data frame with final results for each -- #
+    simple_mid = pd.DataFrame(columns=['Exp 1', 'Exp 2',
+                                       'P. Exp 1', 'P. Exp 2'])
+
+    weighted_mid_a = simple_mid.copy()
+    weighted_mid_b = simple_mid.copy()
+
+    simple_mid['Exp 1'] = apt_mid_e1
+    simple_mid['Exp 2'] = apt_mid_e2
+    simple_mid['P. Exp 1'] = list(np.round(np.array(apt_mid_e1) / np.array(apt_mid_total),2))
+    simple_mid['P. Exp 2'] = 1 - simple_mid['P. Exp 1']
+    simple_mid.index = apt_mid_total.index.values
+    simple_mid.index.name = 'Time'
+
+    weighted_mid_a['Exp 1'] = apt_wmida_e1
+    weighted_mid_a['Exp 2'] = apt_wmida_e2
+    weighted_mid_a['P. Exp 1'] = list(np.round(np.array(apt_wmida_e1) / np.array(apt_mid_total),2))
+    weighted_mid_a['P. Exp 2'] = 1 - weighted_mid_a['P. Exp 1']
+    weighted_mid_a.index = apt_mid_total.index.values
+    weighted_mid_a.index.name = 'Time'
+
+    weighted_mid_b['Exp 1'] = apt_wmidb_e1
+    weighted_mid_b['Exp 2'] = apt_wmidb_e2
+    weighted_mid_b['P. Exp 1'] = list(np.round(np.array(apt_wmidb_e1) / np.array(apt_mid_total),2))
+    weighted_mid_b['P. Exp 2'] = 1 - weighted_mid_b['P. Exp 1']
+    weighted_mid_b.index = apt_mid_total.index.values
+    weighted_mid_b.index.name = 'Time'
+
+    # -- Return data -- #
+    r_data = {'simple_mid_price': simple_mid, 'weighted_mid_price_a': weighted_mid_a,
+              'weighted_mid_price_b': weighted_mid_b}
+
+    return r_data
